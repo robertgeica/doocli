56e12ffccd1c902ee28cbd7cc1f9e85cd6866820
robertgeica <geicarobert@gmail.com>
2021-04-30 16:48:17 +0300

    create board command

--- app.js
+++ app.js
@@ -38,16 +38,16 @@ yargs.command({
 yargs.command({
   command: "login",
   describe: "login user",
-  
+
   async handler(argv) {
     // console.log(argv._[1], argv._[2]);
 
     try {
       const body = { email: argv._[1], password: argv._[2] };
       const res = await axios.post("http://localhost:4000/api/auth", body);
-      
-      keytar.setPassword('doocli', 'user', res.data.token);
-      
+
+      keytar.setPassword("doocli", "user", res.data.token);
+      console.log(res.data.token);
     } catch (error) {
       if (error) {
         console.log("Error", error);
@@ -56,6 +56,33 @@ yargs.command({
   },
 });
 
+yargs.command({
+  command: "cb",
+  describe: "create board",
+
+  async handler(argv) {
+    console.log(argv._[1]);
+
+    const user = keytar.getPassword("doocli", "user");
+
+    try {
+      const boardObj = { boardName: argv._[1], tasks: [] };
+      const token = await user;
+
+      let config = {
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": token,
+        },
+      };
+      await axios.post("http://localhost:4000/api/board", boardObj, config);
+    } catch (error) {
+      if (error) {
+        console.log("Error", error);
+      }
+    }
+  },
+});
 
 
 // stop time
